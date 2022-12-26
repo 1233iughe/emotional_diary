64c63a52d78259ae05f8002933387d6cd9a33f4c
1233iughe <santi28.8.99@gmail.com>
2022-12-25 21:20:42 -0600

    login_required decorator added.

--- diary/helpers.py
+++ diary/helpers.py
@@ -1,7 +1,20 @@
 
-from flask import redirect, render_template, flash
+from flask import redirect, render_template, flash, g, url_for 
+import functools
 
+# Renders and apology with a especified message and code
 def apology(message, code=400):
     # If possible change html template for in-page flash alerts
     # return flash(message=message+''+ str(code),category="error")
     return render_template("error.html", error=code, message=message)
+
+# Decorator that ensures the user is logged
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
